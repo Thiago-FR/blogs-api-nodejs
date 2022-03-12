70a15d8e369d5488ed9ea01aa5cc88cba77660bc
Thiago <thiago_ssfr@hotmail.com>
2022-03-12 17:51:36 -0300

    feat: Req08 and Req09

--- controllers/Categorie.controller.js
+++ controllers/Categorie.controller.js
@@ -1,9 +1,9 @@
 const Service = require('../services/Categories.services');
 
-const findAll = async (_req, res, _next) => {
-  const categorie = await Service.findAll();
+const findAll = async (req, res, _next) => {
+  const user = await Service.findAll();
   
-  res.status(200).json(categorie);
+  res.status(200).json(user);
 };
 
 const createCategorie = async (req, res, _next) => {
--- models/PostsCategorie.js
+++ models/PostsCategorie.js
@@ -7,21 +7,21 @@ const attributes = {
 
 module.exports = (sequelize) => {
   const PostsCategorie = sequelize.define('PostsCategorie', attributes,
-  { timestamps: false, tableName: 'PostsCategories' });
+  { timestamps: false, tableName: 'PostsCategories', underscored: false });
 
   PostsCategorie.associate = (models) => {
     models.BlogPost.belongsToMany(models.Categorie, {
       as: 'categories',
       through: PostsCategorie,
-      foreignKey: 'postId',
-      otherKey: 'categoryId',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
     });
   
     models.Categorie.belongsToMany(models.BlogPost, {
       as: 'posts',
       through: PostsCategorie,
-      foreignKey: 'categoryId',
-      otherKey: 'postId',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
     });
   };
 
